1c7f33a105006eb7b926228deb0bbf99d7465b1a
Nick Thompson <git@nickt.dev>
2022-10-18 23:14:31 -0400

    Initial Commit

---
+++ BeSpooky/BeSpookyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeSpookyApp.swift
+//  BeSpooky
+//
+//  Created by Nick Thompson on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BeSpookyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeSpooky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeSpooky
+//
+//  Created by Nick Thompson on 10/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
